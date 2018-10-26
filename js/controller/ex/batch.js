10751ecee309271e17c96ab6c9a6f054aedbd3da
kaisa <7083620@hanmail.net>
2018-10-26 09:47:53 +0900

    ..

--- js/controller/ex/batch.js
+++ js/controller/ex/batch.js
@@ -20,6 +20,10 @@
 		var now = new Date(),arrayHour = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],arrayMin=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59];
 		var nowHour = parseInt($filter('date')(new Date(),'HH'));
 		var nowMin = parseInt($filter('date')(new Date(),'mm'));
+		/*
+		var b = '2013/09/05 오후 5:15:11';
+		b.replace(/[/:]/gi,'').split(' ');
+		*/
 		$http.get(kaisaApi.getAffList,{params : {} }).then(function(resp){
 			$scope.batch.data = resp.data;
 			for(var i in $scope.batch.data){
