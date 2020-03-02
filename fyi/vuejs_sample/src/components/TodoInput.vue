52f0abbc26685a88b2c496cd109b67eba73af84f
kaisa <kaisa@DESKTOP-5TC4D45>
2020-03-02 09:58:31 +0900

    ..

--- fyi/vuejs_sample/src/components/TodoInput.vue
+++ fyi/vuejs_sample/src/components/TodoInput.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="inputBox shadow">
-      <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo"></input>
+      <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo" />
       <span class="addContainer" v-on:click="addTodo">
         <i class="addBtn fas fa-plus" aria-hidden="true"></i>
       </span>
 
       <modal v-if="showModal" @close="showModal = false">
         <h3 slot="header">경고</h3>
-        <span slot="footer" @click="showModal = false">
-          할 일을 입력하세요.
+        <span slot="footer" @click="showModal = false"> 
+                    할 일을 입력하세요.
           <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
         </span>
       </modal>
